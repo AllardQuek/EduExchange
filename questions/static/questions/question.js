8170b6f59215b3c01907b7d2b0d3bdecf2b34c8a
Allard <AllardQuek@users.noreply.github.com>
2020-05-21 14:20:05 +0800

    voting ui complete

--- questions/static/questions/question.js
+++ questions/static/questions/question.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     upIcons = document.querySelectorAll('.vote-icon');
     upIcons.forEach(icon => {
         icon.onclick = () => {
-            console.log(icon.dataset.ansid);
-            console.log(icon.dataset.vote);
-            // submitVote(icon);
+            console.log(`Ans id: ${icon.dataset.ansid}`);
+            console.log(`Vote: ${icon.dataset.vote}`);
+            submitVote(icon);
         }
     })
 })
@@ -14,11 +14,7 @@ function submitVote(icon) {
     fetch(`/vote/${icon.dataset.ansid}/${icon.dataset.vote}`)
     .then(response => response.text())      // New vote count should be returned
     .then(text => {                         // Take response as input to function
-        console.log(text);
-        if (text.includes("Already")) {
-            return;
-        } else {
-            document.querySelector(`#ans-${icon.dataset.ansid}`).innerHTML = text;
-        }
+        console.log(`New vote count: ${text}`);
+        document.querySelector(`#ans-${icon.dataset.ansid}`).innerHTML = text;
     });
 }
