f0d619eadb8c291a4fb208370e7354e102fb1f21
Allard <AllardQuek@users.noreply.github.com>
2020-05-25 18:57:20 +0800

    connect to s3

--- EduExchange/settings.py
+++ EduExchange/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'storages',         # * For S3 storage
     'django_filters',   # For search functionality
     'mathfilters',      # For doing subtraction easily
     'questions',
@@ -129,6 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # To tweak message types to match Bootstrap's alert classes
@@ -140,3 +142,13 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
+
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
